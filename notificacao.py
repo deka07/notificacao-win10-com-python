a0db242805385a471992adef960837e8cef104b1
ADEMARIO CUNHA <48733312+deka07@users.noreply.github.com>
2021-05-10 15:39:14 -0300

    Add files via upload

---
+++ notificacao.py
@@ -0,0 +1,12 @@
+from win10toast import ToastNotifier
+
+notificacao = ToastNotifier()
+
+notificacao.show_toast('Nome da Aplicação',,
+    'Qualquer mensagem relevante',
+    icon_path=None,
+    duration=10,
+    threaded=True)
+
+while notificacao.notification_active():
+    time.sleep(5)
