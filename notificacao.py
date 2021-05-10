ae7d043616352e98b4002d4d254c974ed0285e59
ADEMARIO CUNHA <48733312+deka07@users.noreply.github.com>
2021-05-10 15:49:46 -0300

    Update notificacao.py

--- notificacao.py
+++ notificacao.py
@@ -1,12 +1,9 @@
 from win10toast import ToastNotifier
+import time
 
 notificacao = ToastNotifier()
 
-notificacao.show_toast('Nome da Aplicação',,
-    'Qualquer mensagem relevante',
-    icon_path=None,
-    duration=10,
-    threaded=True)
+notificacao.show_toast('Nome da Aplicação', 'Qualquer mensagem relevante', icon_path=None, duration=10, threaded=True)
 
 while notificacao.notification_active():
     time.sleep(5)
